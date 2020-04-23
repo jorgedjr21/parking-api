93b024e35430ac65bf115fea7cbdc23b7c3eab7f
Jorge Junior <jorgedjr21@gmail.com>
2020-04-23 16:08:03 -0300

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_23_143958) do
+ActiveRecord::Schema.define(version: 2020_04_23_181948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "parkings", force: :cascade do |t|
     t.string "plate"
-    t.boolean "paid", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "paid_at"
     t.index ["plate"], name: "index_parkings_on_plate"
   end
 
